9fedbc991a0ac1cc7a9ad2dda473ae7a42cc26c5
Abdullah-Byte <abdullahmumuni2002@gmail.com>
2022-03-16 13:56:41 -0700

    C

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+	* times_table - prints the 9 times table
+*/
+void times_table(void)
+{
+	int count = 0;
+	int multiplier;
+	int result;
+
+	while (count < 10)
+	{
+		multiplier = 0;
+												while (multiplier < 10)
+		{
+			if (multiplier > 0)
+				_putchar(' ');								result = count * multiplier;
+			_putchar(result + '0');
+			if (multiplier < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			multiplier++;
+		}
+		_putchar('\n');
+		count++;								}
+}
